309d7ae4d3f39d9fba21d745a942e8348d28d266
tom-shelby <baptiste.catois@gmail.com>
2024-03-19 19:19:46 +0000

    share

--- app/MarketShareRepository.php
+++ app/MarketShareRepository.php
@@ -8,21 +8,27 @@ use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\WebDriverExpectedCondition;
 use Facebook\WebDriver\WebDriverWait;
 
-class MarketShareRepository {
+class MarketShareRepository
+{
 
     // public function __construct()
 
-    public static function loadMarketShare(RemoteWebDriver $driver = null, string $url = null) {
+    public static function loadMarketShare(RemoteWebDriver $driver = null, string $url = null)
+    {
         $buffer = [];
         try {
             $driver->get($url);
             // sleep(1);
 
-            $selector = WebDriverBy::cssSelector('span[data-ist-last]');
+            $dataLastValue = WebDriverBy::cssSelector('span[data-ist-last]');
+            $dataHighValue = WebDriverBy::cssSelector('span[data-ist-last]');
+            $dataLowValue = WebDriverBy::cssSelector('span[data-ist-last]');
+            $dataCloseValue = WebDriverBy::cssSelector('span[data-ist-last]');
+            $dataVolume = WebDriverBy::cssSelector('span[data-ist-last]');
 
-            // $driver->wait(5, 1)->until(WebDriverExpectedCondition::presenceOfElementLocated($selector));
+            $driver->wait(5, 1)->until(WebDriverExpectedCondition::presenceOfElementLocated($dataVolume));
             // $driver->takeScreenshot(storage_path("{$dateStr}_{$code}.png"));
-            $element = $driver->findElement($selector);
+            $element = $driver->findElement($dataLastValue);
 
             // $element = $driver->findElement(WebDriverBy::cssSelector('body'));
             $tagName = $element->getTagName();
@@ -30,15 +36,18 @@ class MarketShareRepository {
 
             $buffer['ist'] = $value;
             dump($value, $tagName);
-        }
-        catch (Exception $exception) {
+        } catch (Exception $exception) {
             throw $exception;
         }
         // finally {
-            // $driver->quit();
+        // $driver->quit();
         // }
 
         return $buffer;
     }
 
+    public static function downloadExcel(RemoteWebDriver $driver = null, string $url = null)
+    {
+
+    }
 }
--- routes/console.php
+++ routes/console.php
@@ -11,6 +11,18 @@ use Laravel\Dusk\DuskServiceProvider;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
+
+    //CommandeAgregerDonnees()
+        //> Connecte la session
+        //> Recuperer toutes les actions depuis la navigation du site
+            //>
+        //> Differentiel avec les actions enregistrées en base: enregistrement en base des nouvelles
+            //> MarketShareRepository::loadMarketShare()
+            //> Verifier (int en int et string en string) données et création d'un MarketShareSnapshot si il existe pas
+            //> Création d'un SnapshotIndex
+            //> Scrap les données pour les actions en base
+            //> A chaque scrap creation d'un MarketShareSnapshot
+
 })->purpose('Display an inspiring quote');
 
 
