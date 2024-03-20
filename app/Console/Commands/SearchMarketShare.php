67b90edcca1c1fce4ec76e8e3b3aceb37d198012
tom-shelby <baptiste.catois@gmail.com>
2024-03-20 15:53:23 +0000

    phpmyadmin + search market shares work

--- app/Console/Commands/SearchMarketShare.php
+++ app/Console/Commands/SearchMarketShare.php
@@ -31,69 +31,98 @@ class SearchMarketShare extends Command
     public function handle(RemoteWebDriver $driver)
     {
         try {
-            $URL = "https://www.boursorama.com/bourse/actions/cotations/";
             $selectorTable = WebDriverBy::cssSelector("table.c-table.c-table--generic.c-table--generic.c-shadow-overflow__table-fixed-column.c-table-top-flop");
             $selectorLinks = WebDriverBy::cssSelector("tr td a");
             $selectorPagination = WebDriverBy::cssSelector('div[role=navigation]');
 
             $selectorPages = WebDriverBy::cssSelector('a');
 
-            $driver->get($URL);
-            $driver->wait(5)->until(WebDriverExpectedCondition::presenceOfElementLocated($selectorPagination));
-            $navigation = $driver->findElement($selectorPagination);
+            $URL = "";
+            $nextURL = "https://www.boursorama.com/bourse/actions/cotations/";
+            $visitedPageLinks = [];
 
-            $pages = $navigation->findElements($selectorPages);
-            // Gérer changement de pages
-            // foreach($pages)
+            do {
+                $URL = $nextURL;
+                $this->info("Scraping market share urls on : {$URL}");
 
-            $table = $driver->findElement($selectorTable);
-            $links = $table->findElements($selectorLinks);
-
-
-
-            // dd($links);
-
-            $buffer = [];
-            foreach($links as $link) {
-                $dataHref = $link->getDomProperty("href");
-                // $dataIsin = $newDriver;
-
-                // $regex_isin = '/\/([0-9a-zA-Z]+)\/$/';
-                // $matches = [];
-                // preg_match($regex_isin, $dataHref, $matches, PREG_OFFSET_CAPTURE, 0);
-                // $dataIsin = $matches[1][0];
-
-                $dataName = $link->getDomProperty("innerText");
+                array_push($visitedPageLinks, $URL);
+                $driver->get($URL);
 
 
-                // $driver->get($dataHref);
-                $selectorIsin = WebDriverBy::cssSelector('h2.c-faceplate__isin');
-                // $dataIsin = $driver->findElement($selectorIsin)->getDomProperty("innerText");
+                $driver->wait(5)->until(WebDriverExpectedCondition::presenceOfElementLocated($selectorPagination));
+                $navigation = $driver->findElement($selectorPagination);
+
+                $pages = $navigation->findElements($selectorPages);
+                // Gérer changement de pages
+                // foreach($pages)
+                $bufferPageLinks = [];
+                $lastURL = $pages[array_key_last($pages)];
+
+                foreach ($pages as $page) {
+                    // dd($page->getTagName());
+                    $pageUrl = $page->getDomProperty('href');
+                    $this->info("Found new possible result result page for market share urls: {$pageUrl}");
+                    if (
+                        !in_array($pageUrl, $visitedPageLinks)
+                    ) {
+                        $this->info("Adding this to future pages to scrap: {$pageUrl}");
+                        array_push($bufferPageLinks, $pageUrl);
+                    } else {
+                        $this->warn("Not adding this to future pages to scrap: {$pageUrl}");
+                    }
+                }
+                $nextURL = $bufferPageLinks[0] ?? null;
+                $this->info("Next page would be: {$nextURL}");
+
+                // dump($bufferPageLinks);
+                // break;
+                // exit;
+
+                $table = $driver->findElement($selectorTable);
+                $links = $table->findElements($selectorLinks);
+
+                // dd($links);
+
+                $buffer = [];
+                foreach ($links as $link) {
+                    $this->info("Scraping links found on: {$URL}");
+                    $dataHref = $link->getDomProperty("href");
+                    $this->info("Found {$dataHref}...");
+                    // $dataIsin = $newDriver;
+
+                    // $regex_isin = '/\/([0-9a-zA-Z]+)\/$/';
+                    // $matches = [];
+                    // preg_match($regex_isin, $dataHref, $matches, PREG_OFFSET_CAPTURE, 0);
+                    // $dataIsin = $matches[1][0];
+
+                    $dataName = $link->getDomProperty("innerText");
+
+
+                    // $driver->get($dataHref);
+                    $selectorIsin = WebDriverBy::cssSelector('h2.c-faceplate__isin');
+                    // $dataIsin = $driver->findElement($selectorIsin)->getDomProperty("innerText");
 
 
 
-                $buffer[] = new MarketShare([
-                    'name'  => $dataName,
-                    // 'isin'  => $dataIsin,
-                    'url'   => $dataHref
-                ]);
-            }
+                    $buffer[] = new MarketShare([
+                        'name'  => $dataName,
+                        // 'isin'  => $dataIsin,
+                        'url'   => $dataHref
+                    ]);
+                }
 
-            foreach ($buffer as $marketShare) {
-                $driver->get($marketShare->url);
-                $selectorIsin = WebDriverBy::cssSelector('h2.c-faceplate__isin');
-                $dataIsin = $driver->findElement($selectorIsin)->getDomProperty("innerText");
+                foreach ($buffer as $marketShare) {
+                    $driver->get($marketShare->url);
+                    $selectorIsin = WebDriverBy::cssSelector('h2.c-faceplate__isin');
+                    $dataIsin = $driver->findElement($selectorIsin)->getDomProperty("innerText");
 
-                $marketShare->isin = $dataIsin;
-                $marketShare->save();
-            }
-
-
-        }
-        catch (Exception $e) {
+                    $marketShare->isin = $dataIsin;
+                    $marketShare->save();
+                }
+            } while ($nextURL !== null);
+        } catch (Exception $e) {
             throw $e;
-        }
-        finally {
+        } finally {
             $driver->quit();
         }
     }
