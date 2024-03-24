8e23670c1cc31e1c6a5d2724adf069203fc69fc9
tom-shelby <baptiste.catois@gmail.com>
2024-03-24 05:40:24 +0000

    cleanup et exemples

--- routes/console.php
+++ routes/console.php
@@ -1,31 +1,11 @@
 <?php
 
-use App\Console\Commands\ScrapeMarketShares;
-use App\Providers\SeleniumServiceProvider;
-use Facebook\WebDriver\Chrome\ChromeDriverService;
-use Facebook\WebDriver\Remote\DesiredCapabilities;
-use Facebook\WebDriver\Remote\RemoteWebDriver;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\Boursorama\Aggregate;
 use Illuminate\Support\Facades\Schedule;
-use Laravel\Dusk\Browser;
-use Laravel\Dusk\DuskServiceProvider;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
 
-    //CommandeAgregerDonnees()
-        //> Connecte la session
-        //> Recuperer toutes les actions depuis la navigation du site
-            //> MarketShare : url, nom, ist
-            //>
-        //> Differentiel avec les actions enregistrées en base: enregistrement en base des nouvelles
-            //> MarketShareRepository::loadMarketShare()
-            //> Verifier (int en int et string en string) données et création d'un MarketShareSnapshot si il existe pas
-            //> Création d'un SnapshotIndex
-            //> Scrap les données pour les actions en base
-            //> A chaque scrap creation d'un MarketShareSnapshot
+Schedule::command(Aggregate::class, ['--fresh', '-n'])->cron("0 0 * * *");
 
-})->purpose('Display an inspiring quote');
+Schedule::command(Aggregate::class, ['-n'])->cron("*/30 * * * *");
 
-// Schedule::command(ScrapeMarketShares::class, [])->cron("*/30 * * * *");
+Schedule::command(Aggregate::class, ['-n', '--ms=OVHCLOUD'])->cron("*/1 * * * *");
--- routes/web.php
+++ routes/web.php
@@ -2,49 +2,10 @@
 
 use App\Http\Controllers\MarketShareSnapshotController;
 use App\Http\Controllers\SnapshotIndexController;
-use App\MarketShareRepository;
-use App\Models\MarketShareSnapshot;
-use Facebook\WebDriver\Remote\DesiredCapabilities;
-use Facebook\WebDriver\Remote\RemoteWebDriver;
-use Facebook\WebDriver\WebDriverBy;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Str;
-
-// Route::get('/', function () {
-    // $codes = [
-    //     '1rPADOC',
-    //     '1rPAB',
-    //     '1rPABCA',
-    //     '1rPAC'
-    // ];
-    // $data = [];
-
-
-    // $seleniumServerUrl = config('selenium.server_url');
-    // $desiredCapabilities = config('selenium.driver_capabilities', DesiredCapabilities::chrome());
-    // $driver = RemoteWebDriver::create($seleniumServerUrl, $desiredCapabilities);
-
-    // //Connexion du driver
-    // $driver->get('https://boursorama.com/');
-    // // $driver->
-
-    // try {
-    //     foreach ($codes as $code) {
-    //         $url = "https://boursorama.com/cours/{$code}";
-    //         $data[$code] = MarketShareRepository::loadMarketShare($driver, $url);
-    //     }
-    // } catch (Exception $e) {
-    //     throw $e;
-    // } finally {
-    //     $driver->quit();
-    //     dd($data);
-    // }
-    // return $data;
-// });
-
 
 Route::get('/', [SnapshotIndexController::class, 'index']);
+
 Route::get('snapshot/{snapshotIndex}/share/{marketShare}', [MarketShareSnapshotController::class, 'index']);
 
 Route::get('market-snapshot-index/{snapshotIndex}', [MarketShareSnapshotController::class, 'index'])
@@ -52,16 +13,3 @@ Route::get('market-snapshot-index/{snapshotIndex}', [MarketShareSnapshotControll
 
 Route::get('market-snapshot/{marketShareSnapshot}', [MarketShareSnapshotController::class, 'show'])
  ->name("market-share-snapshot.view");
-
-
-//  Route::get('/aggregate', function () {
-//     $outputBuffer = new class extends \Symfony\Component\Console\Output\StreamOutput
-//     {
-//         public function __construct()
-//         {
-//             parent::__construct(fopen('php://output', 'w'));
-//         }
-//     };
-
-//     return Artisan::call('boursorama:aggregate -n', outputBuffer: $outputBuffer);
-//  });
