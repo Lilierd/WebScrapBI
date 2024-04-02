a93fa32677042c886a001221162f4ef4a5ec8c0c
tom-shelby <baptiste.catois@gmail.com>
2024-04-02 10:12:17 +0000

    WEB UI

--- resources/views/components/page-component.blade.php
+++ resources/views/components/page-component.blade.php
@@ -8,18 +8,32 @@
     <meta http-equiv="X-UA-Compatible"
           content="ie=edge">
     <title>Document</title>
+
+    @vite(['resources/css/app.css'])
   </head>
 
   <body>
+    <header>
+      <h1> {{ $title }} </h1>
+    </header>
+    <main>
+      <section>
 
-    <h1> {{ $title }} </h1>
+        <iframe
+                height="480px"
+                width="720px"
+                src="{{ env('APP_URL') . ':7900?password=' . env('SAIL_VNC_PASSWORD') . '&autoconnect=1&resize=scale' }}"></iframe>
+      </section>
+      <section>
+        {{-- <div class="overflow-wrapper"> --}}
+            {!! $childComponent !!}
+        {{-- </div> --}}
+      </section>
+    </main>
 
-    {!! $childComponent !!}
-
-    <iframe height="480px"
-            width="720px"
-            src="{{ env('APP_URL') . ':7900?password=' . env('SAIL_VNC_PASSWORD') . '&autoconnect=1&resize=scale' }}"></iframe>
-
+    <footer>
+      <p>©️ EICNAM 2024</p>
+    </footer>
   </body>
 
 </html>
